634c886bfa7cad879a5d806b907b5c7929507540
Tom <contact@jsdevtom>
2018-10-31 05:53:17 +0100

    feature(Seed): seed users in to the database

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Text;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedUsers()
+        {
+            var usersJson = System.IO.File.ReadAllText("Data/UserSeed.json");
+
+            var users = JsonConvert.DeserializeObject<List<User>>(usersJson);
+            
+            users.ForEach(user =>
+            {
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Username = user.Username.ToLower();
+
+                _context.Users.Add(user);
+            });
+
+            _context.SaveChanges();
+        }
+        
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+    
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -32,6 +32,7 @@ namespace DatingApp.API
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -47,7 +48,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -74,6 +75,7 @@ namespace DatingApp.API
             }
 
 //            app.UseHttpsRedirection();
+//            seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
